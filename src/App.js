e8f779dbd6542b01ddaa3ac326f3d1ed803a8e3d
hoanghailongvn <hoanghailong2642k@gmail.com>
2021-12-19 01:04:48 +0700

    create schedule

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-// import HeaderMain from './components/header/headerMain/HeaderMain';
-// import Footer from './components/footer/Footer';
-// import Reviews from './components/reviews/Reviews';
-// import News from './components/news/News';
-// import Speaker from './components/speaker/Speaker';
-// import ScheduleDemo from './components/scheduleDemo/ScheduleDemo'
-// import Introduce from './components/introduce/Introduce'
-// import HeaderSecond from './components/header/headerSecond/HeaderSecond';
-// import HeaderHasAuthor from './components/headerHasAuthor/HeaderHasAuthor';
-// import CalendarNotes from './components/CalendarNotes/CalendarNotes';
-//import Register from './screens/register/Register';
-// import SignIn from './screens/signIn/SignIn';
-import Profile from './components/profile/Profile';
+import Example from './components/createSchedule/CreateSchedule';
+import HeaderMain from './components/header/headerMain/HeaderMain';
+import Footer from './components/footer/Footer';
+import Reviews from './components/reviews/Reviews';
+import News from './components/news/News';
+import Speaker from './components/speaker/Speaker';
+import ScheduleDemo from './components/scheduleDemo/ScheduleDemo'
+import Introduce from './components/introduce/Introduce'
+import HeaderSecond from './components/header/headerSecond/HeaderSecond';
+import HeaderHasAuthor from './components/headerHasAuthor/HeaderHasAuthor';
+ import CalendarNotes from './components/CalendarNotes/CalendarNotes';
+import Register from './screens/register/Register';
+import SignIn from './screens/signIn/SignIn';
+import CreateSchedule from './components/createSchedule/CreateSchedule';
+// import Profile from './components/profile/Profile';
+
+
 
 function App() {
   return (
@@ -24,13 +28,22 @@ function App() {
       <Reviews />
       <News />
       <Footer /> */}
+
+
+
+      
       {/* <HeaderSecond/>  */}
       {/* <HeaderHasAuthor/> */}
       {/* <CalendarNotes /> */}
+
+
+
       {/* <Register/> */}
       {/* <SignIn/> */}
-      <Profile/>
+      {/* <Profile/> */}
 
+     <CalendarNotes/> 
+     {/* <CreateSchedule/> */}
     </div>
   );
 }
--- src/components/CalendarNotes/CalendarNotes.js
+++ src/components/CalendarNotes/CalendarNotes.js
@@ -1,64 +1,167 @@
-import * as React from 'react';
-import 'react-calendar/dist/Calendar.css';
-import { Calendar } from 'react-calendar';
-import { useState } from 'react';
-import Modal from '@mui/material/Modal';
+import React, { useState } from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import { INITIAL_EVENTS, createEventId } from './event-utils'
+import { Modal } from 'react-bootstrap';
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DateTimePicker from '@mui/lab/DateTimePicker';
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import propTypes from 'prop-types';
 
-import './CalendarNotes.scss';
-
-
-const style = {
-    position: 'absolute',
-    top: '15%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 600,
-    bgcolor: 'background.paper',
-    border: '1px solid #fff',
-    boxShadow: 24,
-    p: 4,
-};
 export default function CalendarNotes() {
-    const [value, onChange] = useState(new Date());
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    const [valueDate, setValue] = React.useState(new Date());
+
+  CalendarNotes.propTypes = {
+    onSubmit: propTypes.func,
+
+  }
+  CalendarNotes.defaultProps = {
+    onSubmit: null,
+  }
+
+
+  const [weekendsVisible, setweek] = useState(true);
+  const [currentEvents, setCurrentEvents] = useState([])
+  const [show, setShow] = useState(false);
+
+  const [valueTime, setValueTime] = React.useState(new Date());
+  const [valueTime2, setValueTime2] = React.useState(new Date());
+  const [title, setTitle] = React.useState('');
+
+  const [selectInfo, setSelectInfo] = React.useState('');
+
+  const [timeStart, setTimeStart] = React.useState('');
+  const [timeEnd, setTimeEnd] = React.useState('');
+  
+
+  function handleShow(selectInfo) {
+    setShow(true);
+    setSelectInfo(selectInfo);
+  }
+
+  function handleSave() {
+    setShow(false);
+    if (selectInfo) {
+      let calendarApi = selectInfo.view.calendar
+      calendarApi.unselect()
+      let dateClick = selectInfo.startStr;
+      setTitle('');
+  
+      if (title != '') {
+        calendarApi.addEvent({
+          id: createEventId(),
+          title,
+          start: valueTime.toISOString().slice(0, 19),
+          end: valueTime2.toISOString().slice(0, 19)
+        })
+      }
+    }
+
+  }
+
+  function handleClose(e) {
+    setShow(false);
+    console.log("abc" , valueTime.toISOString().slice(0, 19));
+  }
+
+  function handelValueTitle(e) {
+    setTitle(e.target.value);
+  }
+
+  function renderEventContent(eventInfo) {
     return (
-        <div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DateTimePicker
-                            renderInput={(props) => <TextField {...props} />}
-                            label="DateTimePicker"
-                            value={valueDate}
-                            onChange={(newValue) => {
-                                setValue(newValue);
-                            }}
-                        />
-                    </LocalizationProvider>
-
-                </Box>
-            </Modal>
-            <Calendar
-                onChange={onChange}
-                value={value}
-                onClickDay={handleOpen}
-            />
-        </div>
-
-
+      <>
+        <b>{eventInfo.timeText} &nbsp;</b>
+        <i> {eventInfo.event.title}</i>
+      </>
     )
+  }
 
+  const handleEvents = (events) => {
+    setCurrentEvents(events)
+  }
+
+  function handleTimeEvent(e){
+    console.log(e.target.value)
+
+  }
+
+  return (
+    <div className="time-table" style={{ 'width': '80%', 'margin': '0 auto' }}>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }}
+        initialView='dayGridMonth'
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        weekends={true}
+        initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+        select={handleShow}
+        eventContent={renderEventContent} // custom render function
+        eventsSet={handleEvents}
+      />
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="mt-2 mb-3">
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" value={title} onChange={handelValueTitle} />
+          </div>
+          <div className="mt-2 mb-3">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DateTimePicker
+                renderInput={(props) => <TextField {...props}/>}
+                label="DateTimePicker"
+                value={valueTime}
+                onChange={(newValueTime) => {
+                  setValueTime(newValueTime);
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+          <div className="mt-2 mb-3">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DateTimePicker
+                renderInput={(props) => <TextField {...props} />}
+                label="DateTimePicker"
+                value={valueTime2}
+                onChange={(newValueTime2) => {
+                  setValueTime2(newValueTime2);
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" className="saveChange" onClick={handleSave}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )
 }
+
+
+
+
+
+
+
+
+
